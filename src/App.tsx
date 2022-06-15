b2723ed157b1fd4729eb94507a0cbebca7c7cc89
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-15 17:01:41 -0300

    feat: Criado estilos gerais da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import About from 'components/About';
 import Header from 'components/Header';
+import Nav from 'components/Nav';
 import React from 'react';
 
 export default function App() {
   return (
     <React.Fragment>
       <Header />
+      <About />
+      <Nav />
     </React.Fragment>
   );
 }
